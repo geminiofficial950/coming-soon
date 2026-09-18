46587ff12eeefed4273f453c82e135acbfc5238d
Mohd Shahzil <Mohdshahzil59@gmail.com>
2026-09-18 10:45:39 +0530

    database

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,25 +1,26 @@
 import { MongoClient, type Db } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-  throw new Error("Missing MONGODB_URI in environment variables");
-}
-
 declare global {
   // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-const client = new MongoClient(uri);
-const clientPromise =
-  global._mongoClientPromise ?? client.connect();
+function getClientPromise(): Promise<MongoClient> {
+  const uri = process.env.MONGODB_URI;
 
-if (process.env.NODE_ENV !== "production") {
-  global._mongoClientPromise = clientPromise;
+  if (!uri) {
+    throw new Error("Missing MONGODB_URI in environment variables");
+  }
+
+  if (!global._mongoClientPromise) {
+    const client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+
+  return global._mongoClientPromise;
 }
 
 export async function getDb(): Promise<Db> {
-  const connected = await clientPromise;
+  const connected = await getClientPromise();
   return connected.db();
 }
